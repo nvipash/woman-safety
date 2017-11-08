c7984699d51fd359db6d5ad1f083ae3f94f5b98e
Kulbach <polina.alexia@gmail.com>
2017-11-08 20:35:28 +0200

    Added ListView and selection number

--- app/src/main/java/com/alexia/callbutton/SettingsActivity.java
+++ app/src/main/java/com/alexia/callbutton/SettingsActivity.java
@@ -6,13 +6,15 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
 public class SettingsActivity extends Activity {
     Button settingsButton;
     EditText editText;
-
+    String[] phones = { "0933797479", "0632478177", "0500213201" };
     SharedPreferences preferences;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,11 +27,17 @@ public class SettingsActivity extends Activity {
         preferences = SettingsActivity.this.getSharedPreferences("shared_pref", MODE_PRIVATE);
         final SharedPreferences.Editor editor = preferences.edit();
 
+        final ListView lvMain = (ListView) findViewById(R.id.lvMain);
+        lvMain.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_single_choice, phones);
+        lvMain.setAdapter(adapter);
+
         settingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                editor.putString("phone", editText.getText().toString());
+                editor.putString("phone", phones[lvMain.getCheckedItemPosition()]);
                 editor.commit();
             }
         });
