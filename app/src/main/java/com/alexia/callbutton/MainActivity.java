44abc1aeddcf0e06f30b6de381000e498b68ebb1
Pavel Novitskiy <pasha.6912@gmail.com>
2017-11-13 00:59:37 +0200

    Add BottomNavigationView with 3 base icons
    Add icons for bottom bar with text description and description for
    ListView

--- app/src/main/java/com/alexia/callbutton/MainActivity.java
+++ app/src/main/java/com/alexia/callbutton/MainActivity.java
@@ -8,6 +8,8 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.Fragment;
@@ -16,12 +18,13 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
 
     SharedPreferences preferences;
 
@@ -40,7 +43,6 @@ public class MainActivity extends FragmentActivity {
             }
         });
     }
-
 
     public void dial(View v) {
         if(isPermissionGranted()) {
@@ -100,6 +102,29 @@ public class MainActivity extends FragmentActivity {
 
             // other 'case' lines to check for other
             // permissions this app might request
+
+
         }
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_help:
+
+                            case R.id.action_map:
+
+//                            case R.id.action_sos:
+
+                            case R.id.action_settings:
+
+                        }
+                        return true;
+                    }
+                });
+
     }
 }
