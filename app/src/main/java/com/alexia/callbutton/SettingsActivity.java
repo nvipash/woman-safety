3631f4f640e9c38cece4c399f2faf22272176658
Kulbach <polina.alexia@gmail.com>
2017-11-13 20:53:22 +0200

    Changed SettingsActivity to SettingsFragment

--- app/src/main/java/com/alexia/callbutton/SettingsActivity.java
+++ app/src/main/java/com/alexia/callbutton/SettingsActivity.java
@@ -5,13 +5,14 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends FragmentActivity {
     Button settingsButton;
     EditText editText;
     String[] phones = { "0933797479", "0632478177", "0500213201" };
@@ -19,7 +20,7 @@ public class SettingsActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings_fragment);
+        setContentView(R.layout.settings_activity);
 
         settingsButton = (Button)findViewById(R.id.button2);
         editText = (EditText)findViewById(R.id.editText);
