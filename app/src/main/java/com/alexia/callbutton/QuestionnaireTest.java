51dcacf7ec14a29d4ea280660a847290fac5aaa8
Pavel Novitskiy <novipsh@outlook.com>
2017-12-04 17:48:32 +0200

    Added Spring Boot compability with getters for POST (test functionality)
    HttpRequestTask for QuestionnaireActivity for getting POST info for
    test_info; Added Spring Boot to build.gradle

---
+++ app/src/main/java/com/alexia/callbutton/QuestionnaireTest.java
@@ -0,0 +1,13 @@
+package com.alexia.callbutton;
+
+/**
+ * Created by DELL on 04/12/2017.
+ */
+
+public class QuestionnaireTest {
+    private String question;
+
+    public String getQuestion() {
+        return this.question;
+    }
+}
