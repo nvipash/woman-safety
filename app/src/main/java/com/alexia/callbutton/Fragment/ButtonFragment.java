306e9e9af33c4f85728747d47d819c5c02ea00b1
Marta Liutyk <melodika99@gmail.com>
2018-03-22 11:03:39 +0200

    Added changes to manifest for work with permissions

--- app/src/main/java/com/alexia/callbutton/Fragment/ButtonFragment.java
+++ app/src/main/java/com/alexia/callbutton/Fragment/ButtonFragment.java
@@ -1,4 +1,4 @@
-package com.alexia.callbutton;
+package com.alexia.callbutton.Fragment;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -15,6 +15,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 import android.Manifest;
+
+import com.alexia.callbutton.R;
+
 import static android.support.v4.content.PermissionChecker.checkSelfPermission;
 
 /**
